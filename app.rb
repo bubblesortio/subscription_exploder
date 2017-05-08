68c5a9dcac15ed675b6159e1e4cd4ecf65cb6eaf
Andre Arko <andre@arko.net>
2017-05-08 12:32:43 -0700

    include port number from URL

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ configure do
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
     :host     => db.host,
+    :port     => db.port,
     :username => db.user,
     :password => db.password,
     :database => db.path[1..-1],
